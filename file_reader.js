88c30369df42d43127fc28a7d72ec89b98775479
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-07-24 16:48:38 -0700

    chore: improve performance

--- file_reader.js
+++ file_reader.js
@@ -32,15 +32,18 @@ fs.readFile("firstname.txt", "utf8")
 // ASYNC/AWAIT SOLUTION BELOW THIS LINE
 async function readFiles() {
   try {
-    const firstname = await fs.readFile("firstname.txt", "utf8");
-    const lastname = await fs.readFile("lastname.txt", "utf8");
-    const age = await fs.readFile("age.txt", "utf8");
-    let hobbies = await fs.readFile("hobbies.txt", "utf8");
-    hobbies = JSON.parse(hobbies);
+    const [firstname, lastname, age, hobbies] = await Promise.all([
+      fs.readFile("firstname.txt", "utf8"),
+      fs.readFile("lastname.txt", "utf8"),
+      fs.readFile("age.txt", "utf8"),
+      fs.readFile("hobbies.txt", "utf8"),
+    ]);
+
+    const parsedHobbies = JSON.parse(hobbies);
     console.log(
       `${firstname.trim()} ${lastname.trim()} is ${age.trim()} years old and his hobbies are ${
-        hobbies[0]
-      } and ${hobbies[1]}`
+        parsedHobbies[0]
+      } and ${parsedHobbies[1]}`
     );
   } catch (err) {
     console.error("Error reading files:", err);
